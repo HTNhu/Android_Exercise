92d26265bb33331197404a9c60e5d573a582bd51
TranNhu <hotrannhu@gmail.com>
2019-09-10 23:08:23 +0700

    lÃistgriddialog

---
+++ app/src/main/java/com/example/android_exercise/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.android_exercise;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Dialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnBai1 = findViewById(R.id.btnBai1);
+        Button btnBai2 = findViewById(R.id.btnBai2);
+        Button btnBai3 = findViewById(R.id.btnBai3);
+        Button btnBai4 = findViewById(R.id.btnBai4);
+        Button btnBai5 = findViewById(R.id.btnBai5);
+        btnBai1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, Listview.class));
+            }
+        });
+        btnBai2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, GridviewActivity.class));
+            }
+        });
+        btnBai3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, DialogActivity.class));
+            }
+        });
+//        btnBai4.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                startActivity(new Intent(MainActivity.this, Bai04Activity.class));
+//            }
+//        });
+//        btnBai5.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                startActivity(new Intent(MainActivity.this, Bai5Activity.class));
+//            }
+//        });
+    }
+}
